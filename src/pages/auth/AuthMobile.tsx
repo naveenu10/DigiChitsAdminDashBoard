4255c5276b6e385450522bdffc7a131c2c5233d5
naveenu10 <naveeny@nu10.co>
2023-12-14 19:57:25 +0530

    comments removed

--- src/pages/auth/AuthMobile.tsx
+++ src/pages/auth/AuthMobile.tsx
@@ -22,10 +22,6 @@ const AuthMobile = () => {
     }
   };
 
-  const handleVerifyMobile = () => {
-    setAdminOtp("");
-  };
-
   const handleMobileNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     const mobileNumber = e.target.value;
 
